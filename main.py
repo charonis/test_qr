89c1a259d3374bcc403b50c0901aac32b8606e9c
dfsfd <dfkjakfja>
2024-07-18 20:30:21 +0300

    modified:   __pycache__/main.cpython-312.pyc 	modified:   main.py 	storage/test2.jpg

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
 from fastapi import FastAPI, UploadFile
 from fastapi.responses import FileResponse
+import os
 
 
 app = FastAPI(title="qrtest")
 
 
 @app.get("/get_image")
-async def get_image():
-    return FileResponse("storage/test.jpg")
+async def get_image(index: int):
+    try:
+        content = os.listdir("storage")
+        return FileResponse(f"storage/{content[index]}")
+    except: 
+        return {"error: not find index"}
 
+
+@app.get("/get_all_image")
+async def get_all_image():
+    content = os.listdir("storage")
+    return content
 
 
 @app.post("/post_image")
 async def post_image(file: UploadFile ):
+    content = file.file.read()
+    with open(file=f"storage/{file.filename}", mode="wb", ) as image:
+        os.chdir("storage")
+        image.write(content )
     return {"filename": file.filename}
     
